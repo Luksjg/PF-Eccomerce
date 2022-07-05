1e08928f9b612e0dbb7b119d467d6c75d5e13865
Lucas <Lucasjg696@gmail.com>
2022-07-05 19:13:44 -0300

    reducer 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import "./App.module.css"
 import Home from "./components/home/Home.jsx"
+import {BrowserRouter, Route, Switch} from 'react-router-dom' 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          {/* <Route exact path="/tienda" component={Store}/>  */}
+          {/* <Route path="/tienda/:producto" componente={productDetail}/> */}
+          {/* <Route path="/edit" component={editPage}/>  */}
+          {/* <Route path="/nosotros" component={weAreTheChampions}/> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
