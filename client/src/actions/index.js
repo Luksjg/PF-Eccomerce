71cd4660a33622b8060b10660a32d3a51eeea65c
Lucas <Lucasjg696@gmail.com>
2022-07-05 18:49:50 -0300

    reducer 1.0

---
+++ client/src/actions/index.js
@@ -0,0 +1,84 @@
+import axios from "axios"
+
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS"
+export const GET_BY_CATEGORY = "GET_BY_CATEGORY"
+export const GET_PRODUCT = "GET_PRODUCT"
+export const GET_ALL_USERS = "GET_ALL_USERS"
+export const GET_USER = "GET_USER"
+export const DELETE_USER = "DELETE_USER"
+export const GET_REVIEWS = "GET_REVIEWS"
+export const EDIT_USER = "EDIT_USER" 
+
+export function getAllProducts(){
+    return async function(dispatch){
+        let data = await axios.get('http://localhost:3001/product');
+        return dispatch({
+            type: GET_ALL_PRODUCTS,
+            payload: data
+        })
+    }
+}
+
+export function getAllUsers(){
+    return async function(dispatch){
+        let data = await axios.get('http://localhost:3001/user');
+        return dispatch({
+            type: GET_ALL_PRODUCTS,
+            payload: data
+        })
+    }
+}
+
+export function getReviews(){
+    return async function(dispatch){
+        let data = await axios.get(`http://localhost:3001/review`)
+        return dispatch({
+            type: GET_REVIEWS,
+            payload: data
+        })
+    }
+}
+
+export function getUser(id){
+    return async function(dispatch){
+        let data = await axios.get(`http://localhost:3001/user/${id}` );
+        return dispatch({
+            type: GET_ALL_PRODUCTS,
+            payload: data
+        })
+    }
+}
+
+
+export function deleteUser(id){
+    return async function(dispatch){
+        let data = await axios.delete(`http://localhost:3001/user/${id}`)
+        return dispatch({
+            type: DELETE_USER,
+            payload: data
+        })
+    }
+}
+export function editUserAdmin(id){
+    return async function(dispatch){
+        let data = await axios.put(`http://localhost:3001/user/${id}`)
+        return dispatch({
+            tye: EDIT_USER,
+            payload: data
+        })
+    }
+}
+
+export function getByCategory(category){
+    return{
+        type: GET_BY_CATEGORY,
+        payload: category
+    }
+}
+
+export function getProduct(name){
+    return{
+        type: GET_PRODUCT,
+        payload: name
+    }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,63 @@
+import {GET_ALL_PRODUCTS, GET_BY_CATEGORY, GET_PRODUCT, GET_ALL_USERS, GET_USER, GET_REVIEWS, EDIT_USER_ADMIN, DELETE_USER} from "../actions/index"
 
 const InitialState={
-    ejemplo : []
+    allProducts : [],
+    products: [],
+    product: undefined,
+    users: [],
+    user: undefined,
+    reviews: []
 }
 
 function rootReducer(state = InitialState, action){
     switch(action.type){
-        case "ejemplo":
-            return "hola"
+        case GET_ALL_PRODUCTS:
+            return {
+                ...state,
+                products: action.payload,
+                allProducts: action.payload
+            }
+        case GET_PRODUCT:
+            let productFound = state.allProducts.filter(p => p.name === action.payload)
+            return{
+                ...state,
+                product: productFound
+            }
+        case GET_BY_CATEGORY:
+            let productsFound = state.allProducts.filter(p=> p.category = action.payload )
+            return{
+                ...state,
+                products: productsFound 
+            } 
+        case GET_ALL_USERS:
+            return{
+                ...state,
+                users: action.payload
+            }
+        case GET_USER:
+            return{
+                ...state,
+                user: action.payload
+        }
+        case GET_REVIEWS:
+            return{
+                ...state,
+                reviews: action.payload
+            }
+        case EDIT_USER_ADMIN:
+            return{
+                ...state,
+                user: action.payload
+            }
+        case DELETE_USER:
+            let users = state.users.filter(u=> u.name !== action.payload)
+            return{
+                ...state,
+                users: users
+            }
         default:
-            return "hola"
-    }
+            return state
+        }
 }
 
 export default rootReducer
