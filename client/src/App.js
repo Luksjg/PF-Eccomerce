87915d3caaff2d78bbdeb488a868a2a77acb9873
Lucas <Lucasjg696@gmail.com>
2022-07-07 08:23:41 -0300

    Botones para paginado done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home.jsx"
 import {BrowserRouter, Route, Switch} from 'react-router-dom' 
 import CreateProduct from "./components/createProduct/CreateProduct"
 import ProductDetail from "./components/productDetail/ProductDetail"
+import StorePage from "./components/storePage/StorePage"
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home}/>
-          {/* <Route exact path="/tienda" component={Store}/>  */}
+          <Route exact path="/tienda" component={StorePage}/> 
           <Route path="/tienda/:producto" componente={ProductDetail}/>
           {/* <Route path="/edit" component={editPage}/>  */}
-          {/* <Route path="/crearproducto" component={createProduct}/>  */}
+          <Route path="/crearproducto" component={CreateProduct}/> 
           {/* <Route path="/nosotros" component={weAreTheChampions}/> */}
           <Route exact path="/create" component={CreateProduct}/>
         </Switch>
