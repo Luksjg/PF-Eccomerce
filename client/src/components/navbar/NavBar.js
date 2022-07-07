a9caeac607cb76d8d4a8efff712277b37d54de30
Damebeus <damian.damiebeus@gmail.com>
2022-07-07 17:20:35 -0300

    agregue el about como estructura

---
+++ client/src/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Link} from 'react-router-dom'
+
+
+export default function NavBar(){
+
+    return(
+    <div>
+        <Link to="/"><img src="https://www.pnguniverse.com/wp-content/uploads/2020/12/Marihuana.png" alt="icon" width="100" height="100"></img></Link>
+        <Link to="/"><label>Home</label></Link>
+        <Link to="/nosotros"><label>Nosotros</label></Link>
+        <Link to="/tienda"><label>Tienda</label></Link>    
+    </div>
+    )
+}
