c110998087d996e5f6a51bec9c5ec1fa6c4a6285
Damebeus <damian.damiebeus@gmail.com>
2022-07-07 16:39:53 -0300

    About me agregado

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import Footer from "../footer/Footer"
+import Footer from "../footer/Footer";
 import Subscribe from "../subscribe/Subscribe";
-import PaymentInfo from "../paymentInfo/PaymentInfo"
-import NavBar from "../navbar/NavBar"
+import PaymentInfo from "../paymentInfo/PaymentInfo";
+import NavBar from "../navbar/NavBar";
+import AboutMe from "../aboutMe/AboutMe";
 
-export default function Home(){
-    return(
-        <div>
-            <NavBar/>
-            <div>---------------------------------------------------------------------</div>
-            <div><Subscribe/></div>
-            <div>---------------------------------------------------------------------</div>
-            <div><PaymentInfo/></div>
-            <div>---------------------------------------------------------------------</div>
-            <footer><Footer/></footer>
-            <div>---------------------------------------------------------------------</div>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        ---------------------------------------------------------------------
+      </div>
+      <div>
+        <Subscribe />
+      </div>
+      <div>
+        ---------------------------------------------------------------------
+      </div>
+      <div>
+        <PaymentInfo />
+      </div>
+      <div>
+        ---------------------------------------------------------------------
+      </div>
+      <div>
+        <AboutMe />
+      </div>
+      <div>
+        ---------------------------------------------------------------------
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+      <div>
+        ---------------------------------------------------------------------
+      </div>
+    </div>
+  );
 }
