989aa0814182d2fb0236df133bb07dfabedf941b
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-07-11 07:11:28 -0300

    grideos armados

--- client/src/components/mapProducts/MapProducts.js
+++ client/src/components/mapProducts/MapProducts.js
@@ -1,35 +1,37 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import Paginated from "../paginated/Paginated"
-import ProductCard from "./../productCard/ProductCard"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Paginated from "../paginated/Paginated";
+import ProductCard from "./../productCard/ProductCard";
+import style from "./MapProducts.module.css";
+export default function MapProducts({ productsToShow }) {
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default function MapProducts({productsToShow}){
+  let productsPage = 9;
+  const LastProduct = currentPage * productsPage;
+  const FirstProduct = LastProduct - productsPage;
+  const currentProducts = productsToShow.slice(FirstProduct, LastProduct);
 
-    const [currentPage, setCurrentPage] = useState(1)
-
-    let productsPage = 9
-    const LastProduct = currentPage * productsPage
-    const FirstProduct = LastProduct - productsPage
-    const currentProducts = productsToShow.slice(FirstProduct, LastProduct)
-
-    return(
-        <div>
-         <ul>
-            {  currentProducts.map(product => (
-                <div key={product.id}>
-                    <Link to={'/producto/' + product.id}>
-                    <ProductCard
-                    id={product.id}
-                    img={product.img}
-                    name={product.name}
-                    price={product.price}
-                    stock={product.stock}
-                    />
-                    </Link>
-                </div>
-                ))}
-            </ul>
-            <Paginated allProducts={productsToShow.length} setCurrentPage={setCurrentPage}/>
-        </div>
-    )
+  return (
+    <div>
+      <ul className={style.container}>
+        {currentProducts.map((product) => (
+          <div key={product.id}>
+            <Link to={"/producto/" + product.id}>
+              <ProductCard
+                id={product.id}
+                img={product.img}
+                name={product.name}
+                price={product.price}
+                stock={product.stock}
+              />
+            </Link>
+          </div>
+        ))}
+      </ul>
+      <Paginated
+        allProducts={productsToShow.length}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 }
