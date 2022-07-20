82c214c149aea0b436b4cf22d7dd373b63c2f4c8
tadeoigarate <39782983+tadeoigarate@users.noreply.github.com>
2022-07-20 16:43:11 -0300

    filtros en storePage

--- client/src/components/storePage/StorePage.js
+++ client/src/components/storePage/StorePage.js
@@ -4,12 +4,14 @@ import Categories from "../categories/Categories";
 import Footer from "../footer/Footer";
 import MapProducts from "../mapProducts/MapProducts";
 
-import { getAllProducts } from "../../actions";
+import { getAllProducts, order} from "../../actions";
 
 import style from "./StorePage.module.css";
 import NavStore from "../NavStore/NavStore";
 
 export default function StorePage() {
+  const [order1, setOrder1] = useState("null")
+
   const dispatch = useDispatch();
 
   const productsToShow = useSelector((state) => state.products);
@@ -21,6 +23,12 @@ export default function StorePage() {
   const FirstProduct = LastProduct - productsPage;
   const currentProducts = productsToShow.slice(FirstProduct, LastProduct);
 
+  function handleSortA(e){
+    dispatch(order(e))
+    setOrder1(e)
+    setCurrentPage(1)
+  }
+
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
@@ -28,6 +36,18 @@ export default function StorePage() {
   return (
     <div className={style.container}>
       <NavStore setCurrentPage={setCurrentPage}/>
+
+      <div>
+      <select onChange={(e) => handleSortA(e.target.value)}>
+              <option value='all'>Sorts</option>
+              <option value='az'>Ascendente</option>
+              <option value='za'>Descendente</option>
+              <option value='maxPrice'>Menor precio</option>
+              <option value='minPrice'>Mayor precio</option>
+              {/* <option value='maxValoration'>Mayor valoracion</option>
+              <option value='minValoration'>Menor valoracion</option> */}
+            </select>
+      </div>
 
       <div className={style.columnaContainer}>
         <div className={style.columna}>
