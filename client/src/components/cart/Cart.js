fe79cdd0e0f1a6a4a205de1154b8146ea47c99bb
Lucas <Lucasjg696@gmail.com>
2022-07-28 02:26:31 -0300

    casi

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,141 +1,132 @@
-import { getToCart } from "../../actions";
 
-import style from "./Cart.module.css";
+import React, { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
+import {  guardar, guardarMP } from "../../actions"
+import Footer from "../footer/Footer"
+import NavStore from "../NavStore/NavStore"
 
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-// import { getToCart } from "../../actions"
-import Footer from "../footer/Footer";
-import NavStore from "../NavStore/NavStore";
 
-export default function Cart() {
-  const [auxState, setAuxState] = useState("");
-  const [currentUser, setCurrentUser] = useState("");
-  const [totalPrice, setTotalPrice] = useState(1);
-  const [carrito, setCarrito] = useState();
-  // const [loading,setLoading] = useState(false)
-  // const cart  = useSelector(state=>state.cart)
-  // const dispatch = useDispatch()
+export default function Cart(){
 
-  function changeAmount(product, boolean) {
-    let aux = product.count;
-    // if(currentUser){
-    if (boolean) {
-      product.count += 1;
-      handlePrice();
-    } else {
-      product.count -= 1;
-      handlePrice();
-      // }
+    const [auxState,setAuxState]= useState("")
+    const [currentUser,setCurrentUser] = useState("")
+    const [totalPrice,setTotalPrice] = useState(1)
+    const [carrito,setCarrito] = useState()
+    const dispatch = useDispatch()
+
+    function changeAmount(product,boolean){
+        if(boolean){
+          product.count += 1
+          handlePrice()
+        }else{
+          product.count -= 1
+          handlePrice()
+      }
     }
-  }
 
-  function removeProduct(product) {
-    // console.log(product)
-    let array = carrito.filter((p) => p.id !== product.id);
-    localStorage.setItem("carrito", JSON.stringify(array));
-    setCarrito(array);
-  }
+    
+    function removeProduct(product){
 
-  function cartSubmit() {
-    console.log(carrito);
-    let array = carrito.map((p) => {
-      return {
-        productId: p.id,
-        amount: p.price * p.count,
-        quantity: p.count,
-      };
-    });
-    console.log(array);
-  }
+        let array = carrito.filter(p=>p.id !== product.id)
+        localStorage.setItem("carrito", JSON.stringify(array))
+        setCarrito(array)
 
-  useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    if (currentUser) {
-      setCurrentUser(currentUser);
     }
-    setCarrito(JSON.parse(localStorage.getItem("carrito")));
 
-    handlePrice();
-  }, [auxState]);
+    function cartSubmit(){
+      let array = carrito.map(p=>{
+        return {
+          productId : p.id,
+          amount : p.price * p.count,
+          quantity: p.count
+      }
+      })
 
-  function handlePrice() {
-    let aux = 0;
-    setAuxState(aux);
-    carrito && carrito.map((p) => (aux += p.price * p.count));
-    setTotalPrice(aux);
-  }
+      let arraymp = carrito.map(p=>{
+        return{
+          title: p.name,
+          quantity: p.count,
+          price: p.price,
+        }
+      })
+    
+      dispatch(guardar(array))
+      dispatch(guardarMP(arraymp))
+      // dispatch(orderToMP(arraymp))
+    }
 
-  // } David estuvo aqui
 
-  return (
-    <div>
-      <NavStore />
+    useEffect(()=> {
+      const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+      if(currentUser){
+        setCurrentUser(currentUser)
+      }
+      setCarrito(JSON.parse(localStorage.getItem("carrito")))
+      handlePrice()  
+    },[auxState])
 
-      <div className={style.titulo}>
-        <p>Bienvenido {currentUser.username}</p>
-      </div>
+    
+    function handlePrice(){
+        let aux = 0 
+        setAuxState(aux)
+        carrito && carrito.map(p=>(aux += p.price * p.count ))
+        setTotalPrice(aux)
+    }
 
-      {console.log(carrito)}
-      <div className={style.containerproducts}>
-        <div className={style.container}>
-          {carrito?.length > 0 ? (
-            carrito?.map((product, i) => {
-              return (
-                <div className={style.containerglobal}>
-                  <div key={i} className={style.container2}>
-                    <img
-                      src={product.image}
-                      alt='product imagen'
-                      width='150px'
-                      height='150px'
-                    />
-                    <div className={style.name}>
-                      <label>{product.name}</label>
-                    </div>
-
-                    <div className={style.count}>
-                      <button
-                        onClick={() => changeAmount(product, true)}
-                        disabled={
-                          product.count === product.stock ? true : false
-                        }
-                      >
-                        +
-                      </button>
-
-                      <label>{product.count}</label>
-                      <button
-                        onClick={() => changeAmount(product, false)}
-                        disabled={product.count === 1 ? true : false}
-                      >
-                        -
-                      </button>
-                    </div>
-                    <div className={style.remover}>
-                      <label>Remover</label>
-                      <button onClick={() => removeProduct()}>x</button>
-                    </div>
-                    <div className={style.price}>
-                      <label> ${product.price}</label>
-                    </div>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <div>Loadig</div>
-          )}
+    
+    return(
+        <div>
+            <NavStore/>       
+            <div>---------------------------------------------------</div>
+            <div>
+              {currentUser.userId ?
+              <p>Bienvenido {currentUser.username}</p>
+              :
+              <p>Por favor ingrese a su cuenta para realizar la compra</p>
+              }
+            </div>
+            <div>
+              {handlePrice}
+            {carrito?.length > 0 ? carrito.map((product,i)=>{
+            return(
+              <div key={i}>
+              <img src={product.image} alt="product imagen" width="150px" height="150px"/>
+              <label>{product.name}</label>
+              <label> ${product.price}</label>
+              <button onClick={()=>changeAmount(product,true)} disabled={product.count === product.stock? true : false}>+</button>
+              <label>{product.count}</label>
+              <button onClick={()=>changeAmount(product,false)} disabled={product.count === 1? true : false}>-</button>
+              <label>Remover</label>
+              <button onClick={()=>removeProduct(product)}>x</button>
+            </div>
+            )
+            }):<div>Loadig</div>}
+            </div>
+            <div>
+              total price: {totalPrice}
+            </div>
+            <div>
+            <div>
+            <button onClick={()=> cartSubmit()} disabled={!currentUser.userId ? true: false}>
+              {/* {currentUser.userId ?
+              <Link to={"/formulario"}> */}
+                Siguiente
+              {/* </Link>
+              :
+              <div>
+                Siguiente
+              </div> */}
+              {/* } */}
+              </button>
+            </div>
+            </div>
+            <div>
+              <Link to="/historial">Historial</Link>
+            </div>
+            <div>---------------------------------------------------</div>
+            <Footer/>
         </div>
-      </div>
-      <div className={style.priceTotal}>total price: {totalPrice}</div>
-      <div>{/* <button onClick={()=>handleCart()}>COMPRAR</button> */}</div>
-      {/*      <div>
-        <Link to='/profile'>Historial</Link>
-      </div> */}
-
-      <Footer />
-    </div>
-  );
+    )
+    
 }
