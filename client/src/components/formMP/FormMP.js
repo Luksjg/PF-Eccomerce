ec1a60d49eb17f9edac86ce7f0a2fade1b338c99
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-07-28 09:37:04 -0300

    final

--- client/src/components/formMP/FormMP.js
+++ client/src/components/formMP/FormMP.js
@@ -1,156 +1,159 @@
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
-import {  postShipping, cofirmarCompra } from "../../actions"
-import Footer from "../footer/Footer"
-import NavBar from "../navbar/NavBar"
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { postShipping, cofirmarCompra } from "../../actions";
+import Footer from "../footer/Footer";
+import NavBar from "../navbar/NavBar";
+import style from "./FormMP.module.css";
 export default function FormMP() {
+  const data = useSelector((state) => state.orderToMP);
 
-    
+  const puta = useSelector((state) => state.guardar);
 
-    const data = useSelector(state=>state.orderToMP)
+  const [currentUser, setCurrentUser] = useState("");
 
-    const puta = useSelector(state=>state.guardar)
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if (currentUser) {
+      setCurrentUser(currentUser);
+    }
+  }, []);
 
-    const [currentUser,setCurrentUser] = useState("")
+  const [input, setInput] = useState({
+    calle: "",
+    numero: "",
+    ciudad: "",
+    provincia: "",
+    codigoPostal: "",
+    pisoDepartamento: "",
+  });
+  const dispatch = useDispatch();
 
-    useEffect(()=> {
-        const currentUser = JSON.parse(localStorage.getItem("currentUser"))
-        if(currentUser){
-          setCurrentUser(currentUser)
-        }
-    }, [])
+  function handleChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-   
-
-    const [input, setInput] = useState({
+  async function handleSubmit(e) {
+    if (
+      !input.calle ||
+      !input.numero ||
+      !input.ciudad ||
+      !input.provincia ||
+      !input.codigoPostal
+    ) {
+      e.preventDefault();
+      return alert("Complete todos los campos para poder continuar");
+    } else {
+      e.preventDefault();
+      dispatch(postShipping(input));
+      dispatch(cofirmarCompra(puta, currentUser.userId));
+      alert("Envio completado correctamente");
+      setInput({
         calle: "",
         numero: "",
         ciudad: "",
         provincia: "",
         codigoPostal: "",
-        pisoDepartamento: ""
-    });
-;
-    const dispatch = useDispatch()
-
-    function handleChange(e) {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value,
-        })
-    }   
-
-    async function handleSubmit(e) {
-        if (!input.calle || !input.numero || !input.ciudad || !input.provincia || !input.codigoPostal ) {
-            e.preventDefault();
-            return alert("Complete todos los campos para poder continuar")
-        } else{
-            e.preventDefault();
-            dispatch(postShipping(input));
-            dispatch(cofirmarCompra(puta, currentUser.userId))
-            alert("Envio completado correctamente");
-            setInput({
-              calle: "",
-              numero: "",
-              ciudad: "",
-              provincia: "",
-              codigoPostal: ""
-            })
-            dispatch(cofirmarCompra(puta, currentUser.userId))
-            let array = []
-            localStorage.setItem("carrito", JSON.stringify(array));
-            window.location.href = data
-        }
+      });
+      dispatch(cofirmarCompra(puta, currentUser.userId));
+      let array = [];
+      localStorage.setItem("carrito", JSON.stringify(array));
+      window.location.href = data;
     }
-    return(
-        <div>
+  }
+  return (
+    <div>
+      <div>
+        <br />
+        <br />
+        <div className={style.titulo}>
+          <h1>Formulario de envio</h1>
+        </div>
+        <div className={style.container}>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <div>
-            <br />
-            <br />
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div>
-                    <div>
-                        <h1>Direccion Del Envio</h1>
-                    </div>
-                    <p>Nombre de la Calle:</p>
-                    <input 
-                        type="text"
-                        value={input.calle}
-                        name="calle"
-                        placeholder="Nombre de la Calle"
-                        onChange={e => handleChange(e)}
-                     />
-                </div>
+              <div>
+                <h1>Direccion Del Envio</h1>
+              </div>
+              <p>Nombre de la Calle:</p>
+              <input
+                type='text'
+                value={input.calle}
+                name='calle'
+                placeholder='Nombre de la Calle'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                <p>Numero de la Calle:</p>
-                    <input 
-                        type="text"
-                        value={input.numero}
-                        name="numero"
-                        placeholder="numero de la calle"
-                        onChange={e => handleChange(e)} 
-                    />
-                </div>
+            <div>
+              <p>Numero de la Calle:</p>
+              <input
+                type='text'
+                value={input.numero}
+                name='numero'
+                placeholder='numero de la calle'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                <p>Piso y Departament:</p>
-                    <input 
-                        type="text"
-                        value={input.pisoDepartamento}
-                        name="pisoDepartamento"
-                        placeholder="Opcional"
-                        onChange={e => handleChange(e)} 
-                    />
-                </div>
+            <div>
+              <p>Piso y Departament:</p>
+              <input
+                type='text'
+                value={input.pisoDepartamento}
+                name='pisoDepartamento'
+                placeholder='Opcional'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                <p>Ciudad:</p>
-                    <input 
-                        type="text"
-                        value={input.ciudad}
-                        name="ciudad"
-                        placeholder="Ciudad"
-                        onChange={e => handleChange(e)}
-                    />
-                </div>
+            <div>
+              <p>Ciudad:</p>
+              <input
+                type='text'
+                value={input.ciudad}
+                name='ciudad'
+                placeholder='Ciudad'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                <p>Provincia:</p>
-                    <input 
-                        type="text"
-                        value={input.provincia}
-                        name="provincia"
-                        placeholder="Provincia"
-                        onChange={e => handleChange(e)} 
-                    />
-                </div>
+            <div>
+              <p>Provincia:</p>
+              <input
+                type='text'
+                value={input.provincia}
+                name='provincia'
+                placeholder='Provincia'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                <p>Codigo Postal:</p>
-                    <input 
-                        type="text"
-                        value={input.codigoPostal}
-                        name="codigoPostal"
-                        placeholder="Codigo Postal"
-                        onChange={e => handleChange(e)} 
-                    />
-                </div>
+            <div>
+              <p>Codigo Postal:</p>
+              <input
+                type='text'
+                value={input.codigoPostal}
+                name='codigoPostal'
+                placeholder='Codigo Postal'
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-                <div>
-                </div>
+            <div></div>
 
-                <div>
-                    <button type='submit'>Checkout</button>
-                </div>
-                
-            </form>
-            <br />
-            <br />
+            <div>
+              <button type='submit'>Checkout</button>
+            </div>
+          </form>
         </div>
-            <Footer />
-        </div>
-    )
+        <br />
+        <br />
+      </div>
+      <Footer />
+    </div>
+  );
 }
